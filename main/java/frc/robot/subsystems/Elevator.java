578f9aa6d035a1d4209e6f473084987a3531e461
Yonube <blazer2008.mail@gmail.com>
2025-01-14 20:10:51 -0600

    Fixed the problem with ElevatorMotorLeft not turning blue

--- main/java/frc/robot/subsystems/Elevator.java
+++ main/java/frc/robot/subsystems/Elevator.java
@@ -1,17 +1,20 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElevatorConstants;
 
 public class Elevator extends SubsystemBase {
   private final CANSparkMax ElevatorMotorRight;
+  private final CANSparkMax ElevatorMotorLeft;
 
   public Elevator(int ElevatorMotorRightID, int ElevatorMotorLeftID) {
     ElevatorMotorRight =
-        new CANSparkMax(ElevatorConstants.ElevatorMotorRight, CANSparkMax.MotorType.kBrushless);
+        new CANSparkMax(ElevatorConstants.ElevatorMotorRightID, CANSparkMax.MotorType.kBrushless);
     ElevatorMotorLeft =
-        new CANSparkMax(ElevatorConstants.ElevatorMotorLeft, CANSparkMax.MotorType.kBrushless);
+        new CANSparkMax(ElevatorConstants.ElevatorMotorLeftID, CANSparkMax.MotorType.kBrushless);
   }
 
   // Spins intake motor to intake notes
